231ef7742ce6f332c7a89c0a5e00f88c4c19d029
leeper <thosjleeper@gmail.com>
2014-05-15 11:58:59 +0200

    start on sm_response parsing (#2)

--- R/getresponses.r
+++ R/getresponses.r
@@ -28,13 +28,47 @@ getresponses <- function(
         return(content)
     } else
         lapply(content$data, `class<-`, 'sm_response')
+        return(structure(content$data, class = 'sm_response_list'))
 }
 
 print.sm_response <- function(x, ...){
     if(!is.null(x$respondent_id))
         cat('Responent ID:',x$respondent_id,'\n')
-    if(!is.null(x$questions))
-        cat(length(x$questions),'Questions','\n')
-    # THIS SHOULD PROBABLY DO SOMETHING BETTER
-    return(x)
+    invisible(x)
+}
+
+as.data.frame.sm_response <- function(x, details = NULL, detail_opts = NULL, ...){
+    if(is.null(details)){
+        details <- do.call('surveydetails', c(survey = x$survey_id, detail_opts))
+    } else if(is.character(details)){
+        details <- do.call('surveydetails', c(survey = details[1], detail_opts))
+    } else if(!inherits(details, 'sm_surveydetails')){
+        stop("'details' is not character or an 'sm_surveydetails' object")
+    }
+    qcount <- x$question_count
+    # parse details:
+        # extract all questions from the `question` element in all pages
+            # `heading` is the display text
+            # `type` contains info about each question type
+        # extract all answers from the `answers` elements of each subelement of `question`
+            # `answer_id` is what is recorded in `sm_response`
+            # `text` is the display seen by respondents
+            # `answers` is empty for "open_ended" type questions
+    # parse into dataframe
+        # build dataframe, where variable names are `question_id` and values are `answer_id`
+        # recode responses by looking up `question_id` in details and recoding answers
+        # rename columns to something
+    invisible(x)
+}
+
+as.data.frame.sm_response_list <- function(x, details = NULL, detail_opts = NULL, ...){
+    if(is.null(details)){
+        details <- do.call('surveydetails', c(survey = x[[1]]$survey_id, detail_opts))
+    } else if(is.character(details)){
+        details <- do.call('surveydetails', c(survey = details[1], detail_opts))
+    } else if(!inherits(details, 'sm_surveydetails')){
+        stop("'details' is not character or an 'sm_surveydetails' object")
+    }
+    out <- do.call(rbind, lapply(x, as.data.frame, details = details, ...))
+    return(out)
 }
